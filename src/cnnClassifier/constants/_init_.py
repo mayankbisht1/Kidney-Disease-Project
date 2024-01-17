f30e352d67131e7a36c95b1a963b8f2db0756416
mayankbisht1 <mayankbisht184@gmail.com>
2024-01-17 16:30:15 +0530

    params.yaml added

--- src/cnnClassifier/constants/_init_.py
+++ src/cnnClassifier/constants/_init_.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path  ("config/config.yaml")
-PARAMS_FILE_PATH = Path ("params.yaml")
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
